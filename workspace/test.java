f070ce63bcac3b214757be35d0b2a1e40baf3232
fmench <143030161+fmench@users.noreply.github.com>
2024-08-28 17:10:52 +0000

    commit -m "start"

--- workspace/test.java
+++ workspace/test.java
@@ -1,5 +1,106 @@
-public class test{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class test extends JFrame implements ActionListener{
+
+
+    private JButton[][] buttons = new JButton[3][3];
+    private char currentPlayer = 'X';
+
+    public test() {
+        setTitle("Tic Tac Toe");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3, 3));
+
+        initializeButtons();
+
+        setVisible(true);
+    }
+
+    private void initializeButtons() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                buttons[row][col] = new JButton("");
+                buttons[row][col].setFont(new Font("Arial", Font.PLAIN, 60));
+                buttons[row][col].setFocusPainted(false);
+                buttons[row][col].addActionListener(this);
+                add(buttons[row][col]);
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton buttonClicked = (JButton) e.getSource();
+        if (buttonClicked.getText().equals("")) {
+            buttonClicked.setText(String.valueOf(currentPlayer));
+            if (checkForWin()) {
+                JOptionPane.showMessageDialog(this, "Player " + currentPlayer + " wins!");
+                resetBoard();
+            } else if (isBoardFull()) {
+                JOptionPane.showMessageDialog(this, "The game is a tie!");
+                resetBoard();
+            } else {
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }
+        }
+    }
+
+    private boolean checkForWin() {
+        for (int i = 0; i < 3; i++) {
+            if (buttons[i][0].getText().equals(String.valueOf(currentPlayer)) &&
+                buttons[i][1].getText().equals(String.valueOf(currentPlayer)) &&
+                buttons[i][2].getText().equals(String.valueOf(currentPlayer))) {
+                return true;
+            }
+
+            if (buttons[0][i].getText().equals(String.valueOf(currentPlayer)) &&
+                buttons[1][i].getText().equals(String.valueOf(currentPlayer)) &&
+                buttons[2][i].getText().equals(String.valueOf(currentPlayer))) {
+                return true;
+            }
+        }
+
+        if (buttons[0][0].getText().equals(String.valueOf(currentPlayer)) &&
+            buttons[1][1].getText().equals(String.valueOf(currentPlayer)) &&
+            buttons[2][2].getText().equals(String.valueOf(currentPlayer))) {
+            return true;
+        }
+
+        if (buttons[0][2].getText().equals(String.valueOf(currentPlayer)) &&
+            buttons[1][1].getText().equals(String.valueOf(currentPlayer)) &&
+            buttons[2][0].getText().equals(String.valueOf(currentPlayer))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isBoardFull() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (buttons[row][col].getText().equals("")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void resetBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                buttons[row][col].setText("");
+            }
+        }
+        currentPlayer = 'X';
+    }
+
     public static void main(String[] args) {
-       System.out.println("Hello world!");
+        new test();
     }
 }
+
